4b86c8efab28c765556551ed5500522ac775de11
josuePDL <135852231+josuePDL@users.noreply.github.com>
2024-12-18 11:10:33 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const countdownElement = document.getElementById('countdown');
+const passwordInput = document.getElementById('password');
+const loginForm = document.getElementById('loginForm');
+const myAudio = document.getElementById('myAudio');
+
+// Fecha de cuenta atrás (ajusta según tus necesidades)
+const countDownDate = new Date("January 09, 2025 00:00:00").getTime();
+
+// Contraseña fija (cambia esto por la contraseña deseada)
+const fixedPassword = "Te amo";
+
+// URL de redirección (cambia esto por la URL de tu página de destino)
+const redirectUrl = "https://sites.google.com/view/primer-mes-/p%C3%A1gina-principal";
+
+// Actualiza la cuenta atrás cada segundo
+setInterval(function() {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  // Calcula días, horas, minutos y segundos
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Muestra la cuenta atrás en el elemento HTML
+  countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}, 1000);
+
+// Maneja el envío del formulario
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  if (passwordInput.value === fixedPassword) {
+    // Redirige a la página de destino
+    window.open(redirectUrl, '_blank');
+
+    // Reproduce la canción
+    myAudio.play();
+  } else {
+    alert('Contraseña incorrecta');
+  }
+});
